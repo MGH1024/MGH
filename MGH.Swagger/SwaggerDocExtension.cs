a5257c35142fbc7b3fc623b6afebe1777984e22d
Meysam <Meysam@MGH>
2023-06-05 01:18:04 +0330

    refactor in MGH.Swagger

---
+++ MGH.Swagger/SwaggerDocExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MGH.Swagger;
+
+public static class SwaggerDocExtension
+{
+    public static void AddSwaggerDoc(this SwaggerGenOptions swaggerGenOptions,OpenApiInfo openApiInfo)
+    {
+        swaggerGenOptions.SwaggerDoc(openApiInfo.Version,openApiInfo);
+    }
+}
