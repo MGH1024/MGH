1abf50f2980493f420acf74c5aae94dce8a1914f
Meysam <Meysam@MGH>
2023-06-05 19:09:32 +0330

    add BadRequestException

---
+++ MGH.Exceptions/BadRequestException.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using Microsoft.Extensions.Logging;
+
+namespace MGH.Exceptions;
+
+
+public class BadRequestException : GeneralException
+{
+    public const int ExceptionCode = 120;
+
+    
+    public BadRequestException(string message) : base(message, "",
+        HttpStatusCode.BadRequest)
+    {
+        ErrorCode = ExceptionCode;
+        Level = LogLevel.Warning;
+    }
+
+   
+    public BadRequestException(string message, Exception innerException = null) : base(message, "",
+        innerException, HttpStatusCode.BadRequest)
+    {
+        ValidationErrors =  new List<ValidationResult>(){new(message)};
+        ErrorCode = ExceptionCode;
+        Level = LogLevel.Warning;
+    }
+
+   
+    public IList<ValidationResult> ValidationErrors { get; protected set; }
+}
