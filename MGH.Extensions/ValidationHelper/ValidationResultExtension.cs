827c9c7c2c2ff5edafdbe1f2a6d7240446565c7d
Meysam <Meysam@MGH>
2023-07-11 11:18:54 +0330

    add Extensions package

---
+++ MGH.Extensions/ValidationHelper/ValidationResultExtension.cs
@@ -0,0 +1,22 @@
+﻿using MGH.Exceptions;
+using MGH.Exceptions.Models;
+using FluentValidation.Results;
+
+namespace MGH.Extensions.ValidationHelper;
+
+public static class ValidationResultExtension
+{
+    public static void RaiseExceptionIfRequired(this ValidationResult validationResult)
+    {
+        if (validationResult.IsValid)
+            return;
+
+        var validationErrors =
+            validationResult
+                .Errors
+                .Select(error => new ValidationError(error.PropertyName, error.ErrorMessage))
+                .ToList();
+
+        throw new CustomValidationException(validationErrors);
+    }
+}
