eebed4df6546f427db3ee3559f08a7d1c69ec2a2
Meysam <Meysam@MGH>
2023-09-30 20:11:38 +0330

    addsome new features2

--- EF/MGH.EF.Persistence/Contexts/AppDbContext.cs
+++ EF/MGH.EF.Persistence/Contexts/AppDbContext.cs
@@ -1,24 +1,19 @@
 ﻿using MGH.Domain.Entities;
+using MGH.EF.Persistence.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace MGH.EF.Persistence.Contexts;
 
 public class AppDbContext : DbContext
-{
-    //way one
-    // public ApplicationDbContext()
-    // {
-    // }
-    //
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    // {
-    //     optionsBuilder.UseSqlServer(
-    //         "Data Source=localhost,14333;Initial Catalog=dbMGH.EF.Review;User ID=sa; Password=Abcde@12345;Integrated Security=false;MultipleActiveResultSets=true;Encrypt=false");
-    // } 
-    
-    //way two
+{ 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-    public DbSet<Person> People { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CustomerConfig).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
