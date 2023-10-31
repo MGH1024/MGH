6d4f4162e4915b7abb920ea4bc7bbe3e01fa0209
Meysam <Meysam@MGH>
2023-10-31 18:37:19 +0330

    mgh.core.secuities 1.0.4

--- Core/MGH.Core.Security/Entities/Policy.cs
+++ Core/MGH.Core.Security/Entities/Policy.cs
@@ -5,4 +5,16 @@ namespace MGH.Core.Security.Entities;
 public class Policy :AuditableEntity<int>
 {
     public string Title { get; set; }
+    
+    public virtual ICollection<PolicyOperationClaim> PolicyOperationClaims { get; set; } = null!;
+
+    public Policy(string title)
+    {
+        Title = title;
+    }
+
+    public Policy(int id,string title) :base(id)
+    {
+        Title = title;
+    }
 }
