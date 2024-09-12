f809149bdb789eb64937d21f3b3f395d2a2f2f99
MGH1024 <meysam.ghiasvand@gmail.com>
2024-09-12 16:10:00 +0330

    bugfix

--- Core/MGH.Core.Domain/Outboxes/OutboxMessage.cs
+++ Core/MGH.Core.Domain/Outboxes/OutboxMessage.cs
@@ -4,10 +4,8 @@ namespace MGH.Core.Domain.Outboxes;
 
 public class OutboxMessage : AuditAbleEntity<Guid>
 {
-    public Guid Id { get; set; }
     public string Type { get; set; } = string.Empty;
     public string Content { get; set; } = String.Empty;
-    public DateTime CreatedAt { get; set; }
     public DateTime? ProcessedAt { get; set; }
     public string Error { get; set; }
 }
