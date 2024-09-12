9b4562af495f158d144dd821be09bfef00f07a62
MGH1024 <meysam.ghiasvand@gmail.com>
2024-09-12 15:13:33 +0330

    persistence 2.0.5

--- Core/MGH.Core.Persistence/Base/IUnitOfWork.cs
+++ Core/MGH.Core.Persistence/Base/IUnitOfWork.cs
@@ -5,5 +5,5 @@ public interface IUnitOfWork
     Task<int> CompleteAsync(CancellationToken cancellationToken);
     Task BeginTransactionAsync(CancellationToken cancellationToken);
     Task CommitTransactionAsync(CancellationToken cancellationToken);
-    Task RollbackTransactionAsync(CancellationToken cancellationToken);
+    Task RollbackTransactionAsync(CancellationToken cancellationToken); 
 }
