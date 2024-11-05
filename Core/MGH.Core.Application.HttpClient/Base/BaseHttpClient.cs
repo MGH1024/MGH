8b6b901f3aa3589401febcd86910461a255d5e76
MGH1024 <meysam.ghiasvand@gmail.com>
2024-11-05 21:50:54 +0330

    refactor

---
+++ Core/MGH.Core.Application.HttpClient/Base/BaseHttpClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace MGH.Core.Application.HttpClients.Base;
+
+public class BaseHttpClient(HttpClient httpClient)
+{
+    protected async Task<T> GetAsync<T>(string endpoint)
+    {
+        var response = await httpClient.GetAsync(endpoint);
+        response.EnsureSuccessStatusCode();
+        var json = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<T>(json);
+    }
+}
---
+++ Core/MGH.Core.Application.HttpClient/Configurations/ExternalServiceInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace MGH.Core.Application.HttpClients.Configurations;
+
+public class ExternalServiceInfo
+{
+    public string BaseUrl { get; set; }
+    public int HandleLifeTime { get; set; }
+    public string HttpClientName { get; set; }
+}
