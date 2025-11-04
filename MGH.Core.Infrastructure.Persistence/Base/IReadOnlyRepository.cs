8649daf60c6b2b8c2c0d6f7bbe527b7e87313c83
mghiasvand <mghiasvand@mafpars.com>
2025-11-04 10:50:18 +0330

    add readonly repository

---
+++ MGH.Core.Infrastructure.Persistence/Base/IReadOnlyRepository.cs
@@ -0,0 +1,13 @@
+﻿using MGH.Core.Infrastructure.Persistence.Models.Filters.GetModels;
+using MGH.Core.Infrastructure.Persistence.Models.Paging;
+
+namespace MGH.Core.Infrastructure.Persistence.Base;
+
+public interface IReadOnlyRepository<TEntity, in TKey>
+{
+    Task<TEntity> GetAsync(GetModel<TEntity> getBaseModel, CancellationToken cancellationToken = default);
+    Task<TEntity> GetAsync(TKey id, CancellationToken cancellationToken = default);
+    Task<IPaginate<TEntity>> GetListAsync(GetListModelAsync<TEntity> getListAsyncModel, CancellationToken cancellationToken = default);
+    Task<IPaginate<TEntity>> GetDynamicListAsync(GetDynamicListModelAsync<TEntity> dynamicListAsyncModel, CancellationToken cancellationToken = default);
+    Task<bool> AnyAsync(GetBaseModel<TEntity> getBaseModel, CancellationToken cancellationToken = default);
+}
