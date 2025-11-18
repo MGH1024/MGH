e40c7999c293cec2eeb4f2e4589b3e89559bca1e
mghiasvand <mghiasvand@mafpars.com>
2025-11-18 10:51:57 +0330

    add some proper message in authorization behavior

--- MGH.Core.Application/Pipelines/Authorization/AuthorizationBehavior.cs
+++ MGH.Core.Application/Pipelines/Authorization/AuthorizationBehavior.cs
@@ -1,8 +1,8 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using MGH.Core.CrossCutting.Exceptions.Types;
 using MGH.Core.Infrastructure.Securities.Security.Constants;
 using MGH.Core.Infrastructure.Securities.Security.Extensions;
-using Microsoft.AspNetCore.Http;
 
 namespace MGH.Core.Application.Pipelines.Authorization;
 
@@ -14,30 +14,44 @@ public class AuthorizationBehavior<TRequest, TResponse>(IHttpContextAccessor htt
         var attribute = (RolesAttribute)Attribute.GetCustomAttribute(typeof(TRequest), typeof(RolesAttribute));
         if (attribute == null) 
             return await next();
-        
-        var authHeader = httpContextAccessor?.HttpContext!.Request.Headers["Authorization"];
-        if(string.IsNullOrEmpty(authHeader))
-            throw new AuthorizationException("there is a problem in auth header");
 
-        if(CountWordOccurrences(authHeader, "Bearer")>1)
-            throw new AuthorizationException("there is a problem in auth header");
-        
+        var authHeader = httpContextAccessor?.HttpContext!.Request.Headers["Authorization"];
+        if (string.IsNullOrEmpty(authHeader))
+            throw new AuthorizationException("Authorization header is missing. Please provide a valid 'Authorization: Bearer <token>' header.");
+
+        if (CountWordOccurrences(authHeader, "Bearer") > 1)
+            throw new AuthorizationException("Authorization header contains multiple 'Bearer' tokens. Please provide exactly one token.");
+
+
         var user = httpContextAccessor.HttpContext?.User;
-        if (user == null || !user.Identity!.IsAuthenticated)
-            throw new AuthorizationException("You are not authenticated.");
-        
+        if (user == null)
+            throw new AuthorizationException("No HTTP context or user information found.");
+
+        if (!user.Identity!.IsAuthenticated)
+            throw new AuthorizationException("The user is not authenticated. Please provide a valid JWT token.");
+
         var roles = attribute.Roles;
         var userRoleClaims = httpContextAccessor.HttpContext?.User.ClaimRoles();
-        
-        if (userRoleClaims == null)
-            throw new AuthorizationException("You are not authenticated.");
 
-        var isNotMatchedAUserRoleClaimWithRequestRoles = string.IsNullOrEmpty(userRoleClaims.FirstOrDefault(urc =>
-            urc == GeneralOperationClaims.Admin || roles.Any(role => role == urc)));
-        
+        if (userRoleClaims == null || !userRoleClaims.Any())
+            throw new AuthorizationException("No role claims found in the token. User cannot be authorized.");
+
+        var isNotMatchedAUserRoleClaimWithRequestRoles = string.IsNullOrEmpty(
+            userRoleClaims.FirstOrDefault(urc =>
+                urc == GeneralOperationClaims.Admin || roles.Any(role => role == urc))
+        );
+
         if (isNotMatchedAUserRoleClaimWithRequestRoles)
-            throw new AuthorizationException("You are not authorized.");
-        
+        {
+            var userRoles = string.Join(", ", userRoleClaims);
+            var requiredRoles = string.Join(", ", roles);
+            throw new AuthorizationException(
+                $"The user does not have the required role to perform this action. " +
+                $"User roles: [{userRoles}]; Required roles: [{requiredRoles}]"
+            );
+        }
+
+
         return await next();
     }
     
